c6dcb90e7acb6090054dc2d484de03198946d03f
Alex <askifimlama@gmail.com>
2025-02-10 12:51:31 +0100

    refactored out generic exception handling.

--- services/game_service/utils/data_loader.py
+++ services/game_service/utils/data_loader.py
@@ -6,21 +6,29 @@ logger = logging.getLogger(__name__)
 
 
 def load_csv(path: str) -> pd.DataFrame:
-    """Load a CSV file as a pandas dataframe."""
+    """
+    Load a CSV file as a pandas dataframe.
+
+    Args:
+        path (str): The file path of the CSV dataset.
+
+    Returns:
+        pd.Dataframe: A pandas DataFrame containing the CSV data.
+
+    Raises:
+        FileNotFoundError: If the specified file is not found.
+        ValueError: If the CSV file is empty.
+    """
     logger.info(f"Attempting to load dataset from {path}")
+
     try:
         df = pd.read_csv(path)
-    except FileNotFoundError:
+    except FileNotFoundError as e:
         logger.error(f"Dataset at {path} was not found.")
-        raise FileNotFoundError(f"Dataset at {path} was not found.")
-    except pd.errors.EmptyDataError:
+        raise FileNotFoundError(f"Dataset at {path} was not found.") from e
+    except pd.errors.EmptyDataError as e:
         logger.error(f"Dataset at {path} is empty.")
-        raise ValueError(f"Dataset at {path} is empty.")
-    except Exception as e:
-        logger.exception(f"An error occurred while loading the dataset at {path}: {e}")
-        raise RuntimeError(
-            f"An error occurred while loading the dataset at {path}: {e}"
-        )
+        raise ValueError(f"Dataset at {path} is empty.") from e
 
     logger.info(f"Dataset loaded successfully from {path}, with {len(df)} rows.")
     return df
